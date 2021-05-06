34c20ffbd9e761ee361c2d380936b33f13c7ac64
ademarr <83689294+ademarr@users.noreply.github.com>
2021-05-07 02:20:43 +0300

    Обновили функционал бота.
    1. Переходит на целевой сайт и ходит по ссылкам, с вероятностью 30% переходит в Яндекс.
    2. Вбивает фразу по буквам.
    3. Имеет ограничение на глубину поиска не далее 5 страницы.

--- yandexbot.js
+++ yandexbot.js
@@ -1,33 +1,73 @@
 // ==UserScript==
-// @name         yandexbot 1
+// @name         Bot for Yandex 2
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  yandexbot
-// @author       Alexandr
+// @description  try to take over the world!
+// @author       Александр
 // @match        https://yandex.ru/*
+// @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
 // @icon
 // @grant        none
 // ==/UserScript==
 
-let keywords = ["гобой", "саксофон", "как звучит флейта"];
+let keywords = ["гобой", "саксофон", "как звучит флейта", "барабан"];
 let button = document.getElementsByClassName('button')[0];
-let links = document.links;
+let links = document.querySelectorAll("a.Link");
 let keyword = keywords[getRandom(0, keywords.length)];
+let yandexInput = document.getElementsByName('text')[0];
+let i = 0;
+
 
 if(button !== undefined){
-    document.getElementsByName('text')[0].value = keyword;
-    document.getElementsByClassName('button')[0].click();
-}else{
-    for(let i=0; i<links.length; i++){
-        if (links[i].href.indexOf('xn----7sbab5aqcbiddtdj1e1g.xn--p1ai')!=-1) {
+    let timerId = setInterval(() => {
+        yandexInput.value += keyword[i];
+        i++;
+        if(i == keyword.length) {
+            clearInterval(timerId);
+            button.click();
+        }
+    }, 1000);
+}else if(location.hostname == "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") {
+    console.log("Мы на Музыкалка-онлайн.рф");
+    setTimeout(()=>{
+        let index = getRandom(0,links.length);
+
+        if(getRandom(0,101)>=70) {
+            location.href = "https://yandex.ru/";
+        }
+        if(links[index].href.indexOf('xn----7sbab5aqcbiddtdj1e1g.xn--p1ai')!=-1) {
+            links[index].click();
+        }
+    }, getRandom(2000,3500));
+}
+else{
+    if(document.querySelector('.pager__item_current_yes').textContent == "5") {
+        console.log("мы сюда зашли");
+        location.href = "https://yandex.ru/";
+        // nextYandexPage = false;
+
+    }
+    //
+    if(document.querySelector('.pager__item_current_yes').textContent !== "5") {
+        setTimeout(()=>{
+            document.querySelector('.pager__item_kind_next').click();
+        } ,getRandom(3000,5000));
+    }
+    let nextYandexPage = true;
+    for(let i=0; i<links.length; i++) {
+        if(links[i].href.indexOf('xn----7sbab5aqcbiddtdj1e1g.xn--p1ai')!=-1) {
             let link = links[i];
+            nextYandexPage = false;
             console.log("Нашел фразу" + link);
-            link.removeAttribute('target');
-            link.click();
+            window.location.href = link;
             break;
         }
     }
+
+
 }
+
+
 function getRandom(min,max) {
     return Math.floor(Math.random()*(max-min)+min);
 }
